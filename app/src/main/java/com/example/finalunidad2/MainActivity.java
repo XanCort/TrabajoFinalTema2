987bc27b3a6a51de39e117e474e294aaea20ccaf
XanCort <xan.cortes@gmail.com>
2024-10-08 19:24:12 +0200

    Commit 1

---
+++ app/src/main/java/com/example/finalunidad2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.finalunidad2;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView nuevoTexto = new TextView(this);
+        ((LinearLayout)findViewById(R.id.scroll)).addView(nuevoTexto);
+        nuevoTexto.setText(R.string.app_name);
+        nuevoTexto.setVisibility(View.VISIBLE);
+        nuevoTexto.setBackgroundColor(Color.RED);
+        nuevoTexto.setHeight(12);
+
+    }
+}
